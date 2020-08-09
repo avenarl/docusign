2b3e2a64042b75e223dd414997e8a6fbb6ad91aa
Raven Carlos T. Rodrigo <ravencarlostrodrigo@gmail.com>
2020-08-09 21:08:40 +0800

    Generate react component document

---
+++ app/javascript/components/Document/index.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Index.jsx extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+      <Button variant="primary">Primary</Button>{' '}
+      </React.Fragment>
+    );
+  }
+}
+
+export default Index.jsx
